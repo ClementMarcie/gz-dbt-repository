ff636c65b76152060fc94e83c085fdb652fdba37
Clément Marcié <clementmarcie2@gmail.com>
2025-05-07 09:19:33 +0000

    base finance créée

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,5 +1,7 @@
-select a.orders_id,
-a.date_date,
+select a.*,
+b.shipping_fee,
+b.log_cost,
+b.ship_cost,
 margin + shipping_fee - log_cost - ship_cost as Operational_margin
 from {{ref("int_orders_margin")}} as a
 left join {{ref("stg_gz_raw_data__raw_gz_ship")}} as b
---
+++ models/mart/finance_days.sql
@@ -0,0 +1,11 @@
+select date_date,
+count(orders_id) as nb_transactions,
+sum(revenue) as revenue,
+avg(revenue) as avg_basket,
+sum(margin) as margin,
+sum(purchase_costs) as total_purchase_cost,
+sum(shipping_fee) as total_shipping_fee,
+sum(log_cost) as total_log_cost,
+sum(quantity) as total_quantity
+from {{ref("int_orders_operational")}}
+group by date_date
